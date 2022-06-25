4300507a33d64a2ec96685a4fc5481b0ebd950ff
tjeaneric <ericjohn415@gmail.com>
2022-06-25 11:17:37 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import useHttp from "./hooks/use-http";
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const { isLoading, sendRequest: fetchTasks, error } = useHttp();
+
+  useEffect(() => {
+    const transformTasks = (taskObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      {
+        url: "https://react-movies-f1222-default-rtdb.firebaseio.com/tasks.json",
+      },
+      transformTasks
+    );
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
